1a8ca026c5174e493f3bea64bfb54432f7dbd853
lemuelmgsn <magsinolemuel68@gmail.com>
2025-01-24 17:38:23 +0100

    refactored code

--- user-needs/src/routes/stories/+page.svelte
+++ user-needs/src/routes/stories/+page.svelte
@@ -2,22 +2,97 @@
     import Story from '../../components/layout/story.svelte';
   </script>
   
+
+<header>
+
+    <div class="heading">
+        <a href="/stories"><img src="/icons/back-button-2.svg" alt="backbutton"></a>
+        <h1>All stories</h1>
+    </div>
+
+    <input type="text" placeholder="Search for stories">
+
+    <ul class="nav-ul">
+        <li>
+            <details>
+                <summary>Animal</summary>Animal
+            </details>
+        </li>
+        <li>
+            <details>
+                <summary>Season</summary>Summer
+            </details>
+        </li>
+        <li>
+            <details>
+                <summary>Language</summary>English
+            </details>
+        </li>
+        <li>
+            <details>
+                <summary>Sorting</summary>Length
+            </details>
+        </li>
+    </ul>
+</header>
+
+
   <main>
     <section>
       <Story />
     </section>
   </main>
   
-  <!-- <img src="/nyan-cat.svg" class="unicorn" alt="unicorn">
-  <img src="/nyan-cat2.svg" class="unicorn-2" alt="unicorn">
-  <img src="/nyan-cat.svg" class="unicorn-3" alt="unicorn">
-  <img src="/nyan-cat2.svg" class="unicorn-4" alt="unicorn"> -->
-
-  <img src="/nyan-cat-tilted2.svg" class="unicorn-5" alt="unicorn">
-  <img src="/nyan-cat-tilted.svg" class="unicorn-6" alt="unicorn">
+  <img src="/nyan-cat-tilted2.svg" class="nyan-1" alt="nyan-cat">
+  <img src="/nyan-cat-tilted.svg" class="nyan-2" alt="nyan-cat">
   
 
   <style>
+.heading{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 1.5rem;
+
+    a{
+        align-self: self-start;
+        margin-right: auto;
+    }
+    h1{
+        margin-right: auto;
+        color: white;
+        font-size: 1.3rem;
+    }
+}
+header {
+    position: fixed;
+    z-index: 10;
+    width: 100%; 
+    height: 10vh; 
+}
+input{
+    width: 100%;
+    padding: .5rem;
+    margin-bottom: .75rem;
+}
+.nav-ul{
+    display: grid;
+    color: white;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: .5rem;
+    list-style-type: none;
+}
+.nav-ul > li{
+    background-color: #9264F4;
+    font-size: 1em;
+    padding: .2rem .3rem;
+    border-radius: 5px;
+}
+details > summary{
+    list-style-type: '>';
+}
+
 
   /* Algemene reset en stijlregels */
   * {
@@ -37,42 +112,7 @@
     --card-top-offset: 0.4em;
     background-image: linear-gradient(#471871, #142151);
   }
-  
-  /* Unicorn styling */
-   .unicorn {
-    height: 20rem;
-    width: 20rem;
-    position: fixed;
-    top: 50%;
-    animation: yayy infinite;
-    animation-duration: 3s;
-  }
-  .unicorn-2 {
-    height: 20rem;
-    width: 20rem;
-    position: fixed;
-    top: 20%;
-    animation: yayy2 infinite;
-    animation-duration: 2.5s;
-  }
-  .unicorn-3{
-    height: 7rem;
-    width: 7rem;
-    position: fixed;
-    top: 20%;
-    animation: yayy infinite;
-    animation-duration: 3s;
-    animation-delay: 2s;
-  }
-  .unicorn-4{
-    height: 6rem;
-    width: 6rem;
-    position: fixed;
-    top: 70%;
-    animation: yayy2 infinite;
-    animation-duration: 3s;
-    animation-delay: 2s;
-  }
+
 
   @keyframes yayy{
     from{
@@ -91,9 +131,8 @@
     }
   } 
 
-/* test scroll */
 
-.unicorn-5{
+.nyan-1{
     height: 20rem;
     width: 20rem;
     position: fixed;
@@ -102,12 +141,12 @@
     animation-duration: 3s;
     animation-delay: 2s;
 }
-.unicorn-5 {
+.nyan-1 {
   animation: scrollOut linear forwards;
   animation-timeline: scroll();
 }
 
-.unicorn-6{
+.nyan-2{
     height: 20rem;
     width: 30rem;
     position: fixed;
@@ -117,7 +156,7 @@
     animation-delay: 2s;
     transform: rotateX(45deg);
 }
-.unicorn-6 {
+.nyan-2 {
   animation: scrollOut2 linear forwards;
   animation-timeline: scroll();
 }
@@ -143,7 +182,7 @@
   /* Main layout */
   main {
     height: 190vh;
-    transform: translateY(15vh);
+    transform: translateY(25vh);
   }
   
   section {
@@ -158,67 +197,6 @@
     }
   }
   
-  /* styling for cards */
-  .card__content {
-    box-shadow: 0 0.2em 1em rgba(0, 0, 0, 0.1), 0 1em 2em rgba(0, 0, 0, 0.1);
-    background: #fff;
-    color: #0a0507;
-    border-radius: 10px;
-    overflow: hidden;
-    display: grid;
-    grid-template-columns: repeat(6, 1fr);
-    grid-template-rows: repeat(3, auto);
-    gap: 10px;
-    padding: 10px;
-    height: var(--card-height);
-    transform-origin: 50% 0%;
-  }
-  
-  .story-image img {
-    max-width: 3.75em;
-    min-height: 4em;
-    object-fit: cover;
-  }
-  
-  .story-title {
-    font-size: 10px;
-    font-weight: 600;
-    grid-area: 1 / 3 / 2 / 6;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    overflow: hidden;
-  }
-  
-  .story-summary {
-    grid-area: 2 / 3 / 3 / 7;
-    max-height: 1.5em;
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 2;
-    overflow: hidden;
-  }
-  
-  .story-playtime,
-  .story-icons {
-    display: flex;
-    align-items: center;
-    gap: 8px;
-  }
-  
-  /* Sticky cards */
-  .card {
-    position: sticky;
-    top: 0;
-    height: var(--card-height);
-  }
-  
-  #cards {
-    list-style: none;
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: repeat(var(--numcards), var(--card-height));
-    padding-bottom: calc(var(--numcards) * var(--card-top-offset));
-    margin-bottom: var(--card-margin);
-  }
+
   </style>
   
